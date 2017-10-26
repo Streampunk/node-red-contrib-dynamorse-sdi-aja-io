561ea0c1a63a2ebc557a85b2f72fbea80babb589
Toby Steele <toby@hoodibrass.com>
2017-10-26 17:34:46 +0100

    Initial change to enable cables and audio on Aja input

--- nodes/50-sdi-in.js
+++ nodes/50-sdi-in.js
@@ -25,6 +25,11 @@ function fixBMDCodes(code) {
   return ajatation.bmCodeToInt(code);
 }
 
+//const fs = require('fs');
+//function TEST_write_buffer(buffer) {
+//    output = fs.appendFile('c:\\users\\zztop\\music\\test_aja_out.dat', buffer, 'binary');
+//}
+
 module.exports = function (RED) {
   function SDIIn (config) {
     RED.nodes.createNode(this,config);
@@ -38,42 +43,50 @@ module.exports = function (RED) {
     var node = this;
     var frameCount = 0;
     var grainDuration = ajatation.modeGrainDuration(fixBMDCodes(config.mode));
-    this.tags = {
-      format : [ 'video' ],
-      encodingName : [ 'raw' ],
-      width : [ `${ajatation.modeWidth(fixBMDCodes(config.mode))}` ],
-      height : [ `${ajatation.modeHeight(fixBMDCodes(config.mode))}` ],
-      depth : [ `${ajatation.formatDepth(fixBMDCodes(config.format))}` ],
-      packing : [ ajatation.formatFourCC(fixBMDCodes(config.format)) ],
-      sampling : [ ajatation.formatSampling(fixBMDCodes(config.format)) ],
-      clockRate : [ '90000' ],
-      interlace : [ (ajatation.modeInterlace(fixBMDCodes(config.mode))) ? '1' : '0' ],
-      colorimetry : [ ajatation.formatColorimetry(fixBMDCodes(config.format)) ],
-      grainDuration : [ `${grainDuration[0]}/${grainDuration[1]}`]
+
+    capture.enableAudio(0,0,0);
+
+    this.vtags = {
+      format : 'video',
+      encodingName : 'raw',
+      width : ajatation.modeWidth(fixBMDCodes(config.mode)),
+      height : ajatation.modeHeight(fixBMDCodes(config.mode)),
+      depth : ajatation.formatDepth(fixBMDCodes(config.format)),
+      packing : ajatation.formatFourCC(fixBMDCodes(config.format)),
+      sampling : ajatation.formatSampling(fixBMDCodes(config.format)),
+      clockRate : 90000,
+      interlace : ajatation.modeInterlace(fixBMDCodes(config.mode)),
+      colorimetry : ajatation.formatColorimetry(fixBMDCodes(config.format)),
+      grainDuration : grainDuration
+    };
+    this.atags = {
+      format: 'audio',
+      encodingName: 'L24',
+      clockRate: 48000,
+      channels: 2,
+      blockAlign: 6,
+      grainDuration: grainDuration
     };
     this.baseTime = [ Date.now() / 1000|0, (Date.now() % 1000) * 1000000 ];
-    var nodeAPI = this.context().global.get('nodeAPI');
-    var ledger = this.context().global.get('ledger');
-    var localName = config.name || `${config.type}-${config.id}`;
-    var localDescription = config.description || `${config.type}-${config.id}`;
-    var pipelinesID = config.device ?
-      RED.nodes.getNode(config.device).nmos_id :
-      this.context().global.get('pipelinesID');
-    var source = new ledger.Source(null, null, localName, localDescription,
-      "urn:x-nmos:format:video", null, null, pipelinesID, null);
-    var flow = new ledger.Flow(null, null, localName, localDescription,
-      "urn:x-nmos:format:video", this.tags, source.id, null);
-    nodeAPI.putResource(source).catch(node.warn);
-    nodeAPI.putResource(flow).then(() => {
-        node.log('Flow stored. Starting capture.');
-        capture.start();
-      },
-      node.warn);
+    var cable = { video: [ { tags: this.vtags } ], backPressure: "video[0]" };
+    if (config.audio === true)
+      cable.audio = [ { tags: this.atags } ];
+    this.makeCable(cable);
+
+    var ids = {
+      vFlowID: this.flowID('video[0]'),
+      vSourceID: this.sourceID('video[0]'),
+      aFlowID: (config.audio === true) ? this.flowID('audio[0]') : undefined,
+      aSourceID: (config.audio === true) ? this.sourceID('audio[0]') : undefined
+    };
+
+    console.log(`You wanted audio?`, ids);
 
     this.eventMuncher(capture, 'frame', (video, audio) => {
       //node.log('Received Frame number: ' + ++frameCount);
-      //node.log('**** Payload: ' + payload);
+      //TEST_write_buffer(audio);
 
+      console.log('Event muching', video.length, audio ? audio.length : "no_audio");
       var grainTime = Buffer.allocUnsafe(10);
       grainTime.writeUIntBE(this.baseTime[0], 0, 6);
       grainTime.writeUInt32BE(this.baseTime[1], 6);
@@ -81,8 +94,12 @@ module.exports = function (RED) {
         grainDuration[0] * 1000000000 / grainDuration[1]|0 );
       this.baseTime = [ this.baseTime[0] + this.baseTime[1] / 1000000000|0,
         this.baseTime[1] % 1000000000];
-      return new Grain([video], grainTime, grainTime, null,
-        flow.id, source.id, grainDuration); // TODO Timecode support
+      var va = [ new Grain([video], grainTime, grainTime, null,
+        ids.vFlowID, ids.vSourceID, grainDuration) ]; // TODO Timecode support
+      if (config.audio === true && audio) va.push(
+        new Grain([audio], grainTime, grainTime, null,
+          ids.aFlowID, ids.aSourceID, grainDuration));
+      return va;
     });
 
     capture.on('error', e => {
@@ -93,6 +110,8 @@ module.exports = function (RED) {
       this.close();
       capture.stop();
     });
+
+    capture.start();
   }
   util.inherits(SDIIn, redioactive.Funnel);
   RED.nodes.registerType("sdi-aja-in", SDIIn);
